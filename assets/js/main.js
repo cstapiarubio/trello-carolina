23d6c6d4025f539000898121ae655b58dbf23a0e
CarolinaTapia <cstapiarubio@gmail.com>
2017-05-17 14:28:01 -0400

    hicimos lista y boton

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-main.js
+window.onload=function(){
+		document.getElementById("boton").addEventListener("click", agregar);
+	}
+
+function agregar(){
+
+	//recuperamos el texto ingresado en textarea y limpio textarea
+	var list=document.getElementById("list").value;
+	document.getElementById("list").value="";
+
+	//indicamos donde estaran las tareas recuperadas del textarea
+	var contenedor=document.getElementById("contenedor");
+	}
